157d7154dde132991eed9aa50c6a89e309495a88
gastoniy <279533@student.pwr.edu.pl>
2025-10-30 10:21:16 +0100

    Add registration function on /registration url

--- forum_proj/forum_app/views.py
+++ forum_proj/forum_app/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Issue, Comment
 from django.http import HttpResponseForbidden
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 @login_required
 def dashboard(request):
