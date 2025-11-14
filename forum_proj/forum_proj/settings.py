753e7e188edb1985eba4890b9fb968c826dba72c
mikalay <miko@mikalaydeb.mikalaydeb.com>
2025-11-14 11:14:08 +0100

    removed mounting .env at build time, see explanation in Dockerfile, added default parameters for env for the build time use

--- forum_proj/forum_proj/settings.py
+++ forum_proj/forum_proj/settings.py
@@ -10,12 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_path = BASE_DIR / ".env"
 
 load_dotenv(dotenv_path)
-if os.getenv('DJANGO_ALLOWED_HOSTS') is None: 
-    raise Exception("Error: failed to load environment variables.")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'dummy_key')
 DEBUG = os.getenv('DJANGO_DEBUG', 'False')
 ALLOWED_HOSTS =  os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(sep=",")
 
@@ -63,11 +61,11 @@ WSGI_APPLICATION = 'forum_proj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME', 'dummy'),
+        'USER': os.getenv('DB_USER', 'dummy'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'dummy'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
@@ -78,10 +76,11 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = []
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = os.getenv('DJANGO_TIME_ZONE')
+TIME_ZONE = os.getenv('DJANGO_TIME_ZONE', 'UTC')
 USE_I18N = True
 USE_TZ = True
 
+# need to ference those in ingress later
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
