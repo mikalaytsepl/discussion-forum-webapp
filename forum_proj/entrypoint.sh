a175de868c435eecb732ada7dbfffa59c06dccca
mikalay <miko@mikalaydeb.mikalaydeb.com>
2025-11-21 00:53:02 +0100

    db creation is now done with the job (commented nc part in entrypoint for that since it wont work wiht ARNs. resolved misconfig with configmap and secret TODO: figure out allowed hosts, get ingress/domain/hpa working, ssl certs

--- forum_proj/entrypoint.sh
+++ forum_proj/entrypoint.sh
@@ -5,10 +5,11 @@ DB_HOST=${DB_HOST:-db}
 DB_PORT=${DB_PORT:-3306}
 
 echo "Waiting for database at $DB_HOST:$DB_PORT..."
-
-while ! nc -z $DB_HOST $DB_PORT; do
-  sleep 0.1
-done
+sleep 10
+echo "slepped for 10 seconds, proceeding with migrations"
+#while ! nc -z $DB_HOST $DB_PORT; do
+#  sleep 0.1
+#done
 
 echo "Database Started"
 
